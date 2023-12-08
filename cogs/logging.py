0109cec39941a414ac055e67a09d7567730818fc
Toasts <61860115+Dracozure@users.noreply.github.com>
2023-12-08 13:22:48 -0800

    Removed check for message content
    Message content does not contain attachments

--- cogs/logging.py
+++ cogs/logging.py
@@ -10,53 +10,52 @@ class Logging(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        if before.content != after.content:
-            author = after.author
-            date_format = "%Y-%m-%d %H:%M:%S"
-            my_timezone = "PST"
-            direct_attachments = await self.processAttachments(before)
+        author = after.author
+        date_format = "%Y-%m-%d %H:%M:%S"
+        my_timezone = "PST"
+        direct_attachments = await self.processAttachments(before)
 
-            timestamp_original = after.created_at.astimezone(timezone("US/Pacific")).strftime(date_format)
-            timestamp_edit = datetime.now().astimezone(timezone("US/Pacific")).strftime(date_format)
+        timestamp_original = after.created_at.astimezone(timezone("US/Pacific")).strftime(date_format)
+        timestamp_edit = datetime.now().astimezone(timezone("US/Pacific")).strftime(date_format)
 
-            author_info_str = f"""
-            Author: <@{author.id}>
-            User ID: {author.id}
-            User Name: {author.name}"""
+        author_info_str = f"""
+        Author: <@{author.id}>
+        User ID: {author.id}
+        User Name: {author.name}"""
 
-            timestamp_info_str = f"""
-            Timezone: {my_timezone}
-            Created At: {timestamp_original}
-            Edited At: {timestamp_edit}"""
+        timestamp_info_str = f"""
+        Timezone: {my_timezone}
+        Created At: {timestamp_original}
+        Edited At: {timestamp_edit}"""
 
-            message_info_str = f"""
-            --__Message Link__--
-            {after.jump_url}
-            --__Message Before__--
-            {before.content}
-            --__Message After__--
-            {after.content}"""
+        message_info_str = f"""
+        --__Message Link__--
+        {after.jump_url}
+        --__Message Before__--
+        {before.content}
+        --__Message After__--
+        {after.content}"""
 
-            attachments_info_str = f"""
-            --__Original Attachments__--
-            {direct_attachments}"""
+        attachments_info_str = f"""
+        --__Original Attachments__--
+        {direct_attachments}"""
 
-            embed = discord.Embed(title = "MESSAGE EDIT ⚠️", color = author.color)
-            
-            embed.add_field(name = "Author Info", value = author_info_str, inline = False)
-            embed.add_field(name = "Timestamp Info", value = timestamp_info_str, inline = False)
-            embed.add_field(name = "Message Info", value = message_info_str, inline = False)
-            embed.add_field(name = "Direct Attachments Info", value = attachments_info_str, inline = False)
+        embed = discord.Embed(title = "MESSAGE EDIT ⚠️", color = author.color)
+        
+        embed.add_field(name = "Author Info", value = author_info_str, inline = False)
+        embed.add_field(name = "Timestamp Info", value = timestamp_info_str, inline = False)
+        embed.add_field(name = "Message Info", value = message_info_str, inline = False)
+        embed.add_field(name = "Direct Attachments Info", value = attachments_info_str, inline = False)
 
-            guild_id = after.guild.id
-            channel = None
+        guild_id = after.guild.id
+        channel = None
 
-            if guild_id == 774455931442298901:
-                channel = self.bot.get_channel(1182578368890290257)
-            else:
-                channel = self.bot.get_channel(after.channel.id)
+        if guild_id == 774455931442298901:
+            channel = self.bot.get_channel(1182578368890290257)
+        else:
+            channel = self.bot.get_channel(after.channel.id)
 
-            await channel.send(embed = embed)
+        await channel.send(embed = embed)
 
     async def processAttachments(self, message):
         attachments = ""
