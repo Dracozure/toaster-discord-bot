14d88c09167c1595ed31096c2f0cb36514a9273a
Toasts <61860115+Dracozure@users.noreply.github.com>
2023-12-12 23:01:13 -0800

    Reset database

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -38,7 +38,7 @@ class Dictionary(commands.Cog):
         if (message.author.bot):
             return
 
-        if (guild_id != 520337076659421192):
+        if (guild_id != 774455931442298901 and channel_id != 1144334325765120143):
             return
         
         try:
