ab5d0b6c5367c1b8d58a3e2fd9f1b2be6943f3bd
Toasts <61860115+Dracozure@users.noreply.github.com>
2023-12-13 15:41:01 -0800

    Add check for same author

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -29,6 +29,7 @@ class Dictionary(commands.Cog):
         date_format = "%Y-%m-%d %H:%M:%S"
 
         author_id = message.author.id
+        last_author_id = await self.get_last_author()
         message_link = message.jump_url
         timestamp = message.created_at.astimezone(timezone("US/Pacific")).strftime(date_format)
 
@@ -39,6 +40,11 @@ class Dictionary(commands.Cog):
             return
 
         if (guild_id != 520337076659421192):
+            return
+        
+        if (last_author_id != "" and int(author_id) == int(last_author_id)):
+            await message.channel.send("Wait for someone else to input word")
+
             return
         
         # USE THE MESSAGE CONTENT INSTEAD, JUST CHECK FOR RESPONSE, MERRIAM WEBSTER RETURNING SOME WEIRD STUFF
@@ -60,29 +66,25 @@ class Dictionary(commands.Cog):
 
             if (word[0] != starting_letter and starting_letter != ""): 
                 await message.add_reaction(wrong_reaction)
-                
                 await message.channel.send(f"Your word must start with the letter **{starting_letter}**")
 
                 return
 
             if (not word_already_typed):
-                await message.add_reaction(correct_reaction)
+                word = await self.trim_word_alpha(word)
 
+                await message.add_reaction(correct_reaction)
                 await self.insert_word(word, author_id, timestamp, message_link)
-                
                 await self.save_last_word(word)
+                await self.save_last_author(author_id)
             else:
                 await message.add_reaction(wrong_reaction)
-                
                 await message.channel.send(f"**{message.content}** has already been typed")
         except:
             await message.add_reaction(wrong_reaction)
-
             await message.channel.send(f"**{message.content}** is invalid")
 
     async def insert_word(self, word, author_id, timestamp, message_link):
-        word = self.trim_word_alpha(word)
-        
         self.c.execute(f"INSERT INTO words VALUES ('{word}', '{author_id}', '{timestamp}','{message_link}')")
 
         self.conn.commit()
@@ -107,15 +109,29 @@ class Dictionary(commands.Cog):
 
         return last_word[-1]
     
+    async def get_last_author(self):
+        last_author_id = open("./last_author.txt", "r").read().strip()
+
+        if (os.stat("./last_author.txt").st_size == 0):
+            return ""
+
+        return last_author_id
+    
     async def save_last_word(self, word):
         file='./current_word.txt' 
 
         with open(file, 'w') as filetowrite:
             filetowrite.write(word)
 
+    async def save_last_author(self, author_id):
+        file='./last_author.txt'
+
+        with open(file, 'w') as filetowrite:
+            filetowrite.write(str(author_id))
+
     async def trim_word_alpha(self, word):
         for char in word:
-            if (not word.isalpha()):
+            if (not char.isalpha()):
                 word = word.replace(char, "")
 
         return word
